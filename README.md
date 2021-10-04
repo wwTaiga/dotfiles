083f59b61c42ae5fc7cc8f88c6eb152d4ba5fc51
Taiga <qqtaiga@gmail.com>
2021-10-04 09:55:17 -0700

    add tmux and zsh config

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 #My Dotfiles
 
+## Requirements
+    curl
+    git
+    zsh
+
+## Config
+    chsh -s $(which zsh)  "change zsh as default
+
 ## Prerequisite - Install nix package manager
     curl -L https://nixos.org/nix/install | sh
     nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
--- home.nix
+++ home.nix
@@ -9,22 +9,47 @@
             TERMINFO_DIRS = "${pkgs.kitty.terminfo.outPath}/share/terminfo";
         };
         packages = with pkgs; [
+            cargo
+            fzf
+            gcc
+            gnumake
+            go
             neovim
             lazygit
             ripgrep
+            rustc
             tmux
         ];
+
+        file = {
+            ".zshrc" = {
+                source = ./config/zsh;
+            };
+        };
     };
 
-
     programs = {
+        git = {
+            enable = true;
+            userName = "Taiga";
+            userEmail = "qqtaiga@gmail.com";
+        };
         home-manager = {
             enable = true;
         };
     };
 
-    xdg.configFile.nvim = {
-        source = ./config/neovim;
-        recursive = true;
+    xdg = {
+        configFile = {
+            nvim = {
+                source = ./config/neovim;
+                recursive = true;
+            };
+            tmux = {
+                source = ./config/tmux;
+                recursive = true;
+            };
+        };
     };
+
 }
