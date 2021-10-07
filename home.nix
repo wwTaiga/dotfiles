d3e32e98f982795bd90bd9197a7a95aa0313917f
Taiga <qqtaiga@gmail.com>
2021-10-08 01:58:07 +0800

    nix: add st

--- home.nix
+++ home.nix
@@ -26,6 +26,21 @@
             nodePackages.npm
 
             (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
+            (st.overrideAttrs (oldAttrs: rec {
+                buildInputs = oldAttrs.buildInputs ++ [ harfbuzz ];
+                patches = [
+                    (fetchpatch {
+                        url = "https://st.suckless.org/patches/ligatures/0.8.4/st-ligatures-20210824-0.8.4.diff";
+                        sha256 = "0mh7p3k5fx84n80m639agk838x2pp6628w8sca1xjgv64dfic574";
+                    })
+                    (fetchpatch {
+                        url = "https://st.suckless.org/patches/undercurl/st-undercurl-0.8.4-20210822.diff";
+                        sha256 = "091ixzrcqzh156zmrmma8wj6js770l8fdx467rkndy3x6hnbrxj3";
+                    })
+                ];
+                configFile = writeText "config.def.h" (builtins.readFile ./config/st/config.h);
+                postPatch = "${oldAttrs.postPatch}\n cp ${configFile} config.def.h";
+            }))
         ];
 
         file = {
